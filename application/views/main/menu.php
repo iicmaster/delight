c29e2794c01c438df180cb1f6ea2833abe2d1455
iicmaster@iicmaster-PC <iicmaster@live.com>
2013-02-04 19:41:16 +0700

    Cleanup files

--- application/views/main/menu.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="/products">The Pastry shop</a></li>
-    <li><a href="/about">About us</a></li>
-    <li><a href="services.php">Services</a></li>
-    <li><a href="blog.php">Blog</a></li>
-    <li><a href="contact.php">Contact Us</a></li>
-</ul>
